633ef515803a033605d8ac5225ee88011dd656e9
Alan Iwi <alan.iwi@stfc.ac.uk>
2014-09-06 18:13:55 +0100

    remove scratch directory

--- scratch/type_dep_protos.h
+++
@@ -1,7 +0,0 @@
-#include "datatype.h"
-
-/* prototypes of functions for which both single 
- * and double precision versions are compiled.
- */
-
-void FUNC(multiply)(const REAL *, const REAL *, REAL *);
